2e4aa08912bdb46632dc8a0627e28d910c6b22c6
Azadeh Galidari <agalidari@gmail.com>
2023-05-19 15:37:32 +0200

    add Usercomponent and using props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 
+//user component
+const UserCompo = (props) => {
+    return (
+        <div>
+            <h2>name: {props.name}</h2>
+            <h3>age: {props.age}</h3>
+            <h4>email: {props.email}</h4>
+        </div>
+    );
+};
+
 function App() {
     return (
         <div className="App">
             <h2>React proj pedro</h2>
+            <UserCompo name="pedro" age={23} email="pedro@pedro.com" />
+            <UserCompo name="zi" age={32} email="zi@zi.com" />
         </div>
     );
 }
