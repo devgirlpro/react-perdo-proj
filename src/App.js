2ec9d77df2b179de83d9bf749ccea58d0c028232
Azadeh Galidari <agalidari@gmail.com>
2023-05-19 16:27:09 +0200

    add Job component and using props

--- src/App.js
+++ src/App.js
@@ -11,12 +11,26 @@ const UserCompo = (props) => {
     );
 };
 
+//job component
+const Job = (props) => {
+    return (
+        <div>
+            <h2>salary: {props.salary}</h2>
+            <h3>position: {props.position}</h3>
+            <h4>company: {props.company}</h4>
+        </div>
+    );
+};
+
 function App() {
     return (
         <div className="App">
             <h2>React proj pedro</h2>
             <UserCompo name="pedro" age={23} email="pedro@pedro.com" />
+            <Job salary={5500} position="senior" company="Amazon" />
+            <br />
             <UserCompo name="zi" age={32} email="zi@zi.com" />
+            <Job salary={2500} position="junior" company="newName" />
         </div>
     );
 }
