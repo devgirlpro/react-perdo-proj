d331deb04e7fe85dd8bfce52f970c2496d2d3b16
Azadeh Galidari <agalidari@gmail.com>
2023-05-26 20:24:10 +0200

    using fetch & asios

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import "./App.css";
-import { useState } from "react";
-import { Text } from "./Text";
+import Axios from "axios";
+import async from "hbs/lib/async";
+import { useState, useEffect } from "react";
 
 function App() {
-    const [showText, setShowText] = useState(false);
+    const [catFact, setCatFact] = useState("");
+    const [catFactList, setCatFactList] = useState([]);
+
+    /*  fetch("https://catfact.ninja/fact")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data==>", data);
+        }); */
+
+    /*     useEffect(() => {
+        Axios.get("https://catfact.ninja/fact").then((res) => {
+            setCatFact(res.data.fact);
+        });
+    }, []); */
+
+    const fetchCatFact = () => {
+        Axios.get("https://catfact.ninja/fact").then((res) => {
+            setCatFact(res.data.fact);
+        });
+    };
 
     return (
         <div className="App">
-            <button
-                onClick={() => {
-                    setShowText(!showText);
-                }}
-            >
-                Show Text
-            </button>
-
-            {showText && <Text />}
+            <button onClick={fetchCatFact}>Generate cat fact</button>
+            <p>{catFact}</p>
+            <div className="catFactList">
+              
+            </div>
         </div>
     );
 }
