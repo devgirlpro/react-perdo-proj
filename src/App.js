928c58667b3dfb638946dff07793ebbe4309a383
Azadeh Galidari <agalidari@gmail.com>
2023-05-26 15:54:57 +0200

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { useState } from "react";
+import { Text } from "./Text";
 
 function App() {
+    const [showText, setShowText] = useState(false);
+
     return (
         <div className="App">
-            <h2>React proj pedro</h2>
+            <button
+                onClick={() => {
+                    setShowText(!showText);
+                }}
+            >
+                Show Text
+            </button>
+
+            {showText && <Text />}
         </div>
     );
 }
